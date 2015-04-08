0b85408be7a819ab8a45d64b59be372e199b56b1
Rob Trangucci <robert.trangucci@gmail.com>
2015-04-08 19:07:53 -0400

    Now handles (0 + x|group) correctly

--- R/glimmer.R
+++ R/glimmer.R
@@ -17,110 +17,144 @@ concat <- function( ... ) {
 coerce_index <- function( x ) as.integer(as.factor(as.character(x)))
 
 nobars <- function(term) {
-		if (!('|' %in% all.names(term))) return(term)
-		if (is.call(term) && term[[1]] == as.name('|')) return(NULL)
-		if (length(term) == 2) {
-		nb <- nobars(term[[2]])
-		if (is.null(nb)) return(NULL)
-		term[[2]] <- nb
-		return(term)
-		}
-		nb2 <- nobars(term[[2]])
-		nb3 <- nobars(term[[3]])
-		if (is.null(nb2)) return(nb3)
-		if (is.null(nb3)) return(nb2)
-		term[[2]] <- nb2
-		term[[3]] <- nb3
-		term
+  if (!('|' %in% all.names(term))) return(term)
+  if (is.call(term) && term[[1]] == as.name('|')) return(NULL)
+  if (length(term) == 2) {
+    nb <- nobars(term[[2]])
+    if (is.null(nb)) return(NULL)
+    term[[2]] <- nb
+    return(term)
+  }
+  nb2 <- nobars(term[[2]])
+  nb3 <- nobars(term[[3]])
+  if (is.null(nb2)) return(nb3)
+  if (is.null(nb3)) return(nb2)
+  term[[2]] <- nb2
+  term[[3]] <- nb3
+  term
 }
 
 findbars <- function(term) {
-		if (is.name(term) || !is.language(term)) return(NULL)
-		if (term[[1]] == as.name("(")) return(findbars(term[[2]]))
-		if (!is.call(term)) stop("term must be of class call")
-		if (term[[1]] == as.name('|')) return(term)
-		if (length(term) == 2) return(findbars(term[[2]]))
-		c(findbars(term[[2]]), findbars(term[[3]]))
+  if (is.name(term) || !is.language(term)) return(NULL)
+  if (term[[1]] == as.name("(")) return(findbars(term[[2]]))
+  if (!is.call(term)) stop("term must be of class call")
+  if (term[[1]] == as.name('|')) return(term)
+  if (length(term) == 2) return(findbars(term[[2]]))
+  c(findbars(term[[2]]), findbars(term[[3]]))
 }
 
 subbars <- function(term)
 ### Substitute the '+' function for the '|' function
 {
-		if (is.name(term) || !is.language(term)) return(term)
-		if (length(term) == 2) {
-		term[[2]] <- subbars(term[[2]])
-		return(term)
-		}
-		stopifnot(length(term) >= 3)
-		if (is.call(term) && term[[1]] == as.name('|'))
-		term[[1]] <- as.name('+')
-		for (j in 2:length(term)) term[[j]] <- subbars(term[[j]])
-		term
+  if (is.name(term) || !is.language(term)) return(term)
+  if (length(term) == 2) {
+  term[[2]] <- subbars(term[[2]])
+  return(term)
+  }
+  stopifnot(length(term) >= 3)
+  if (is.call(term) && term[[1]] == as.name('|'))
+  term[[1]] <- as.name('+')
+  for (j in 2:length(term)) term[[j]] <- subbars(term[[j]])
+  term
 }
 
 hasintercept <- function(term) {
-		attr( terms(term) , "intercept" )==1
+    attr( terms(term) , "intercept" )==1
 }
 
 clean_name <- function(ranef_expr){
-	return(undot(deparse(ranef_expr)))
+  return(undot(deparse(ranef_expr)))
 }
 
 expand_slash <- function(ranef_expr){
-	ranef_name <- clean_name(ranef_expr[[3]])
-	var <- list()
-	if(regexec(pattern = '/', fixed = TRUE, text = ranef_name) == -1) {
-		return(ranef_expr)
-	} else {
-		vars <- strsplit(ranef_name,split = '/',fixed=TRUE)[[1]]
-		len_vars <- length(vars)
-		if(len_vars > 2)
-			stop('Only supports maximum of 1 "/"')
-		interact_ranef_name <- paste(vars[1],vars[2],sep=':')
-		ranef_expr[[3]] <- as.name(vars[1])
-		interact_to_bars <- findbars(as.formula(paste('~ (1 | ',interact_ranef_name, ')', sep='')))
-	}
-	return(list(ranef_expr,interact_to_bars))
+  ranef_name <- clean_name(ranef_expr[[3]])
+  var <- list()
+  if(regexec(pattern = '/', fixed = TRUE, text = ranef_name) == -1) {
+    return(ranef_expr)
+  } else {
+    vars <- strsplit(ranef_name,split = '/',fixed=TRUE)[[1]]
+    len_vars <- length(vars)
+    if(len_vars > 2)
+      stop('Only supports maximum of 1 "/"')
+    interact_ranef_name <- paste(vars[1],vars[2],sep=':')
+    ranef_expr[[3]] <- as.name(vars[1])
+    interact_to_bars <- findbars(as.formula(paste('~ (1 | ',interact_ranef_name, ')', sep='')))
+  }
+  return(list(ranef_expr,interact_to_bars))
 }
 
 corr_re_test <- function(ranef_expr){
-	if(length(ranef_expr[[2]]) == 1){
-	 return(TRUE)	
-	} else if(deparse(ranef_expr[[2]][[2]]) != '0'){
-		return(TRUE)
-	} else {
-		return(FALSE)
-	}
+  if(length(ranef_expr[[2]]) == 1){
+   return(TRUE) 
+  } else if(deparse(ranef_expr[[2]][[2]]) != '0'){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
 }
 
 build_ranefs <- function(var_list, corr_list, data){
-	num_re <- length(var_list)
-	names_re <- sapply(var_list, function(i) clean_name(i[[3]]))
+  names_re <- sapply(var_list, function(i) clean_name(i[[3]]))
+  names_re_list <- unique(names_re)
+  num_re <- length(names_re_list)
   ranef <- sapply(1:num_re,function(i) setNames(vector(mode='list',length=2),c('corr','uncorr')),simplify=FALSE)
-	ranef <- setNames(ranef,names_re)
-	group_inds_list <- list()
-	for(i in 1:num_re){
-		v <- var_list[[i]][[2]]
-		name <- names_re[i]
-		corr <- corr_list[[i]]
-		v_class <- class(v)
-		if (v_class =="numeric") {
-			if (corr){
-				ranef[[ name ]]$corr <- c("(Intercept)",ranef$corr[[name]])
-			} else {
-				ranef[[name]]$uncorr <- c("(Intercept)",ranef$uncorr[[name]])
-			}
-		} else {
-				f <- as.formula( paste( "~-1+" , deparse( v ) , sep="" ) )
-				if(corr){
-					ranef[[ name ]]$corr <- c(ranef[[name]]$corr,colnames( model.matrix( f , data ) ))
-				} else {
-					ranef[[ name ]]$uncorr <- c(ranef[[name]]$uncorr,colnames( model.matrix( f , data ) ))
-				}
-		}
-		group_inds_list[[name]] <- with(data, eval(var_list[[i]][[3]]))
-	}
-	return(list(ranef = ranef, group_inds_list = group_inds_list))
+  ranef <- setNames(ranef,names_re_list)
+  group_inds_list <- list()
+  for(i in 1:length(var_list)){
+    v <- var_list[[i]][[2]]
+    name <- names_re[i]
+    corr <- corr_list[[i]]
+    v_class <- class(v)
+    if (v_class =="numeric") {
+      if (corr){
+        ranef[[ name ]]$corr <- c("(Intercept)",ranef$corr[[name]])
+      } else {
+        ranef[[name]]$uncorr <- c("(Intercept)",ranef$uncorr[[name]])
+      }
+    } else {
+        f <- as.formula( paste( "~-1+" , deparse( v ) , sep="" ) )
+        if(corr){
+          ranef[[ name ]]$corr <- c(ranef[[name]]$corr,colnames( model.matrix( f , data ) ))
+        } else {
+          ranef[[ name ]]$uncorr <- c(ranef[[name]]$uncorr,colnames( model.matrix( f , data ) ))
+        }
+    }
+    group_inds_list[[name]] <- with(data, eval(var_list[[i]][[3]]))
+  }
+  return(list(ranef = ranef, group_inds_list = group_inds_list))
+}
+
+build_name <- function(re, prefix, group_var, num_group_vars) {
+  aterm <- undot(re)
+  newterm <- ""
+  var_prefix <- prefix[2]
+  if (num_group_vars>1) var_prefix <- concat(var_prefix, group_var, "_")
+  if (aterm == "Intercept") {
+      par_name <- concat(var_prefix, aterm)
+      newterm <- concat(par_name, "[", group_var, "]")
+  } else {
+      par_name <- concat(var_prefix, aterm)
+      newterm <- concat(par_name, "[", group_var, "]", "*", aterm)
+  }
+  return(list(newterm = newterm, par_name = par_name))
+}
+
+build_priors <- function(member, group_var) {
+  prior_list <- list()
+  gvar_name <- concat(member, "[", group_var, "]")
+  prepend <- concat(member,'_')
+  prior_list[[gvar_name]] <- concat("dnorm(0,sigma_", prepend, group_var, ")")
+  prior_list[[concat("sigma_",prepend,group_var)]] <- concat("dcauchy(0,2)")
+  return(prior_list)
+}
+
+apply_build_priors <- function(members, group_var) {
+  prior_list <- list()
+  if(length(members) < 1)
+    return(prior_list)
+  for (i in 1:length(members))
+    prior_list <- c(prior_list, build_priors(members[[i]], group_var))
+  return(prior_list)
 }
 
 xparse_glimmer_formula <- function( formula , data ) {
@@ -135,7 +169,7 @@ xparse_glimmer_formula <- function( formula , data ) {
     
     ## gets names of fixed  effects from colums of of data frame output by model.matrix
     
-		des_frame_fixef <- as.data.frame(model.matrix( f_nobars , data ))
+    des_frame_fixef <- as.data.frame(model.matrix( f_nobars , data ))
     fixef <- names(des_frame_fixef)  
     
     # convert to all fixed effects and build needed model matrix
@@ -156,13 +190,13 @@ xparse_glimmer_formula <- function( formula , data ) {
     } else {
         # find varying effects list
         var <- findbars(formula)
-				var_mod <- sapply(var, function(i) expand_slash(i))
-				var_mod <- unlist(var_mod, recursive=FALSE)
-				corr_uncorr <- sapply(var_mod, function(i) corr_re_test(i))
-				ranef <- build_ranefs(var_mod, corr_uncorr, data)
-				inds <- do.call(cbind,ranef$group_inds_list)
-				names(inds) <- names(ranef$group_inds_list)
-				des_frame_fixef <- cbind(des_frame_fixef,inds)
+        var_mod <- sapply(var, function(i) expand_slash(i))
+        var_mod <- unlist(var_mod, recursive=FALSE)
+        corr_uncorr <- sapply(var_mod, function(i) corr_re_test(i))
+        ranef <- build_ranefs(var_mod, corr_uncorr, data)
+        inds <- do.call(cbind,ranef$group_inds_list)
+        names(inds) <- names(ranef$group_inds_list)
+        des_frame_fixef <- cbind(des_frame_fixef,inds)
     }
     
     # result sufficient information to build Stan model code
@@ -258,43 +292,37 @@ glimmer <- function( formula , data , family=gaussian , prefix=c("b_","v_") , de
     num_group_vars <- length(pf$ranef)
     if ( num_group_vars > 0 ) {
     for ( i in 1:num_group_vars ) {
-        group_var <- undot(names(pf$ranef)[i])
-        members <- list()
-				ranef <- c(pf$ranef[[i]]$corr,pf$ranef[[i]]$uncorr)
-        for (j in 1:length(ranef)) {
-            aterm <- undot(ranef[[j]])
-            newterm <- ""
-            var_prefix <- prefix[2]
-            if (num_group_vars>1) var_prefix <- concat(var_prefix, group_var, "_")
-            if (aterm == "Intercept") {
-                par_name <- concat(var_prefix, aterm)
-                newterm <- concat(par_name, "[", group_var, "]")
-            } else {
-                par_name <- concat(var_prefix, aterm)
-                newterm <- concat(par_name, "[", group_var, "]", "*", aterm)
-            }
-            members[[par_name]] <- par_name
-            if ( i > 1 | j > 1 ) vlm <- concat( vlm , " +\n        " )
-            vlm <- concat( vlm , newterm )
-            print(paste('Iteration:', i, j))
-						print(vlm)
-						print(newterm)
-        }#j
-        # add group prior
-#				print(paste('Iteration: ',i))
-#				print(members)
-        if ( length(members)>1 ) {
-            # multi_normal
-            gvar_name <- concat( "c(" , paste(members,collapse=",") , ")" , "[" , group_var , "]" )
-            prior_list[[gvar_name]] <- concat( "dmvnorm2(0,sigma_" , group_var , ",Rho_" , group_var , ")" )
-            prior_list[[concat("sigma_",group_var)]] <- concat( "dcauchy(0,2)" )
-            prior_list[[concat("Rho_",group_var)]] <- concat( "dlkjcorr(2)" )
+      group_var <- undot(names(pf$ranef)[i])
+      corr_members <- list()
+      uncorr_members <- list()
+      ranef <- c(pf$ranef[[i]]$corr,pf$ranef[[i]]$uncorr)
+      num_corr <- length(pf$ranef[[i]]$corr)
+      num_uncorr <- length(pf$ranef[[i]]$uncorr)
+      for (j in 1:length(ranef)) {
+        name_list <- build_name(ranef[[j]], prefix, group_var, num_group_vars)
+        par_name <- name_list$par_name
+        newterm <- name_list$newterm
+        if (j <= num_corr){
+          corr_members[[par_name]] <- par_name
         } else {
-            # normal
-            gvar_name <- concat( members[[1]] , "[" , group_var , "]" )
-            prior_list[[gvar_name]] <- concat( "dnorm(0,sigma_" , group_var , ")" )
-            prior_list[[concat("sigma_",group_var)]] <- concat( "dcauchy(0,2)" )
+          uncorr_members[[par_name]] <- par_name
         }
+        if ( i > 1 | j > 1 ) vlm <- concat( vlm , " +\n        " )
+        vlm <- concat( vlm , newterm )
+      }#j
+        # add group prior
+      if (length(corr_members)>1) {
+        # multi_normal
+        gvar_name <- concat( "c(" , paste(corr_members,collapse=",") , ")" , "[" , group_var , "]" )
+        prior_list[[gvar_name]] <- concat( "dmvnorm2(0,sigma_" , group_var , ",Rho_" , group_var , ")" )
+        prior_list[[concat("sigma_",group_var)]] <- concat( "dcauchy(0,2)" )
+        prior_list[[concat("Rho_",group_var)]] <- concat( "dlkjcorr(2)" )
+      } else {
+          prior_list_uncorr_members <- apply_build_priors(uncorr_members,group_var)
+          if (length(corr_members) == 1)
+            prior_list_corr_members <- build_priors(corr_members[[1]], group_var)
+          prior_list <- c(prior_list, prior_list_corr_members, prior_list_uncorr_members)
+      }
         # make sure grouping variables in dat
         # also ensure is an integer index
         pf$dat[[group_var]] <- coerce_index(pf$dat[,group_var])
